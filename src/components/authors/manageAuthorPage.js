fa2b44459b39745c5ec04367d97e2fb73647a03e
koziscool <kozjob@yahoo.com>
2016-10-23 12:35:49 -0500

    stuff

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -13,14 +13,24 @@ var ManageAuthorPage = React.createClass({
         Router.Navigation
     ],
 
+    statics: {
+        willTransitionFrom: function( transition, component) {
+            if( component.state.dirty && !confirm('leave without saving?') ) {
+                transition.abort();
+            }
+        }
+    },
+
     getInitialState: function() {
         return {
             author: { id: '', firstName: '', lastName: '' },
-            errors: {}
+            errors: {},
+            dirty: false
         };
     },
 
     setAuthorState: function( event ) {
+        this.setState({dirty: true});
         var field = event.target.name;
         var value = event.target.value;
         this.state.author[field] = value;
@@ -51,6 +61,7 @@ var ManageAuthorPage = React.createClass({
             return;
         }
         AuthorApi.saveAuthor(this.state.author);
+        this.setState({dirty: false});
         toastr.success('Author saved.');
         this.transitionTo('authors');
     },
