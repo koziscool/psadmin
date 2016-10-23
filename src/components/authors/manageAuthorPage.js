7e8d1d81b6068f26e60d8d6a6e93e7dfb4aafa5f
koziscool <kozjob@yahoo.com>
2016-10-23 11:51:00 -0500

    toastr notification

--- src/components/authors/manageAuthorPage.js
+++ src/components/authors/manageAuthorPage.js
@@ -2,10 +2,17 @@
 "use strict";
 
 var React = require('react');
+var Router = require('react-router');
 var AuthorForm = require('./authorForm');
 var AuthorApi = require('../../api/authorApi');
+var toastr = require('toastr');
+
 
 var ManageAuthorPage = React.createClass({
+    mixins: [
+        Router.Navigation
+    ],
+
     getInitialState: function() {
         return {
             author: { id: '', firstName: '', lastName: '' }
@@ -22,6 +29,8 @@ var ManageAuthorPage = React.createClass({
     saveAuthor: function(event) {
         event.preventDefault();
         AuthorApi.saveAuthor(this.state.author);
+        toastr.success('Author saved.');
+        this.transitionTo('authors');
     },
 
     render: function() {
